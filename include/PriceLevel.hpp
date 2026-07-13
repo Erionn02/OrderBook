a08d0a12bb92c74aac9d534a487d896de57480d7
Erionn02 <jkjablonski@gmail.com>
2026-07-14 00:09:12 +0200

    Changed order's alignment, pre-allocated levels and removed unnecessary test_bit checks in packed_memory_array

--- include/PriceLevel.hpp
+++ include/PriceLevel.hpp
@@ -5,7 +5,23 @@
 #include <list>
 #include <boost/pool/pool_alloc.hpp>
 
-using OrderListAllocator = boost::fast_pool_allocator<Order, boost::default_user_allocator_new_delete, boost::details::pool::null_mutex>;
+template<typename T>
+struct AlignedNewDelete
+{
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+
+    static char * malloc (const size_type bytes) noexcept
+    {
+        return static_cast<char*>(operator new(bytes, std::align_val_t(alignof(T)), std::nothrow));
+    }
+    static void free(char * const block) noexcept
+    {
+        operator delete(block, alignof(T));
+    }
+};
+
+using OrderListAllocator = boost::fast_pool_allocator<Order, AlignedNewDelete<Order>, boost::details::pool::null_mutex>;
 
 struct PriceLevel {
     Price price;

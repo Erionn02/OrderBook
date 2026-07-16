fe984289cf11e4b4f03e93313a98cd03ae431690
Erionn02 <jkjablonski@gmail.com>
2026-07-17 00:20:48 +0200

    Improved readme

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ then
     build_dir="cmake-build-$(echo "$1" | awk '{print tolower($0)}')"
 fi
 mkdir -p $build_dir
-conan install . --output-folder=$build_dir --build=missing -s build_type=$build_type
-cd $build_dir && cmake -G Ninja .. -DCMAKE_BUILD_TYPE=$build_type "${@:2}" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake && cmake --build . -- -j $(nproc --all)
+conan install . --output-folder=$build_dir --build=missing -s build_type=$build_type -pr:b=clang-profile -pr:h=clang-profile
+cd $build_dir
+source conanbuild.sh
+cmake -G Ninja .. -DCMAKE_BUILD_TYPE=$build_type "${@:2}" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake && cmake --build . -- -j $(nproc --all)

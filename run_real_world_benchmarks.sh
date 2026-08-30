833c674d786396c99a409a59a9a90fd2819948c8
Erionn02 <106995561+Erionn02@users.noreply.github.com>
2026-08-30 13:52:26 +0200

    Fixed latency measurements and improved benchmarking environment (#30)

--- run_real_world_benchmarks.sh
+++ run_real_world_benchmarks.sh
@@ -20,5 +20,6 @@ if [ ! -f ./cache/${test_ticker}.itch ]; then
 fi
 
 echo "performance" | sudo tee /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+sleep 1
 sudo env LD_PRELOAD=${LD_PRELOAD} MALLOC_CONF=${MALLOC_CONF} taskset -c 3 chrt -f 99 ./cmake-build-release/RealWorldDataOrderBookBenchmark ./cache/${test_ticker}.itch "${@:2}"
 echo "powersave" | sudo tee /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
